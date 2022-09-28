4199843bcbaa0ac63f94503dec28a203700722ab
Seth Yates <29878+sethyates@users.noreply.github.com>
2022-09-28 11:25:29 -0700

    fix: smoketest

--- test/smoketest/callback_test.go
+++ test/smoketest/callback_test.go
@@ -1,3 +1,5 @@
+//go:build !unit && !integration && smoke
+
 package smoketest
 
 import (
--- test/smoketest/results_test.go
+++ test/smoketest/results_test.go
@@ -1,3 +1,5 @@
+//go:build !unit && !integration && smoke
+
 package smoketest
 
 func (suite *SmokeTestSuite) TestResults() {}
