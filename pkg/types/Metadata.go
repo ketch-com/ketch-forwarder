9502b1b9cd5f7e0ee039f529760c0719296eb2fd
Navaneeth <32714420+neeth04@users.noreply.github.com>
2022-11-29 14:31:31 -0800

    chore(docs): update doc to reflect uid change

--- pkg/types/Metadata.go
+++ pkg/types/Metadata.go
@@ -3,7 +3,6 @@ package types
 import (
 	"context"
 	validation "github.com/go-ozzo/ozzo-validation/v4"
-	"github.com/go-ozzo/ozzo-validation/v4/is"
 )
 
 type Metadata struct {
@@ -13,7 +12,6 @@ type Metadata struct {
 
 func (r *Metadata) ValidateWithContext(ctx context.Context) error {
 	return validation.ValidateStructWithContext(ctx, r,
-		validation.Field(&r.UID, validation.Required, is.UUID),
 		validation.Field(&r.Tenant, validation.Required),
 	)
 }
